ca191e9dd4e74d94a6f2c5913da6601a3644873e
lkilgoretrout <lkilgoretrout@yahoo.com>
2023-06-15 13:00:27 -0700

    findDuplicates: corrected output and updated binary

--- findDuplicates.c
+++ findDuplicates.c
@@ -27,6 +27,27 @@ void findDuplicates (struct Array arr)
     }
 }
 
+void countDuplicates (struct Array arr)
+{
+    int count = 0, j=0;
+    for (int i=0; i<arr.size; i++)
+    {
+	    if (arr.A[i]==arr.A[i+1])
+        {
+            j = i+1;
+            while (arr.A[j]==arr.A[i])
+                j++;
+          
+            printf("\n'%d' was found %d times ", arr.A[i], j-i);
+            i = j-1;
+        }
+    }
+}
+
+
+            	    
+
+
 int main()
 {
     // create user defined int array
@@ -39,16 +60,17 @@ int main()
     int* A = (int *) malloc(size * sizeof(int));
     for (int i=0; i<size; i++)
     {
-	printf("\nnumber %d: ", i+1);
-	int val = -1;      
+	    printf("\nnumber %d: ", i+1);
+	    int val = -1;      
         scanf("%d", &val);
-	A[i] = val; 
+	    A[i] = val; 
     }
 
     struct Array arr = { A, size };
 
     findDuplicates(arr);
-   
+    countDuplicates(arr);
+
     return 0;
 }	     
 
