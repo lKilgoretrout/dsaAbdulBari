66afba7fd9fe7eae07e3829586906eb75d7922a9
Robert Baucus <lkilgoretrout@yahoo.com>
2023-07-12 12:06:43 -0700

    added some comments explaining row-major and column-major ordering

--- lowerTriangularMatrix.c
+++ lowerTriangularMatrix.c
@@ -9,14 +9,25 @@
  *  4 a41  a42  a43  a44  0
  *  5 a51  a52  a53  a54  a55
  *
- * number of non-zero elements: (n(n+1))/ 2 
+ * number of non-zero elements: (n(n+1))/ 2  (sum of n natural numbers) 
  * number of zero     elements: n^2 - ((n(n+1))/2)
  *
  * row-major array representation:
  *     A = [ a11  a21  a22  a31  a32 a33 a41 a42 a43 a44 a51 a52 a53 a54 a55]
- *   row =   1    2    2    3    3   3   4   4   4   4   5   5   5   5   5    
- *  
- *  M[row, column] -> A[i, j] = ( (i(i-1))/2 ) + j - 1
+ *   row =   1    2    2    3    3   3   4   4   4   4   5   5   5   5   5        
+ * 
+ * for 1 based matrix index formula into 0-based index in Array:
+ * M[i][j] --> A[i][j] : 
+ *     M[row, column] -> A[i, j] = ( (i(i-1))/2 ) + j - 1
+ *
+ * column-major array representation:
+ *     A = [ a11  a21  a32  a41  a51 a22 a32 a42 a52 a33 a43 a53 a44 a54 a55]
+ *   row =   1    1    1    1    1   3   4   4   4   4   5   5   5   5   5    
+ *  A[i] =   0    1    2    3    4   5   6   7   8   9   10  11  12  13  14
+ *  M[row, column] -> A[i, j]:
+ *       m[4][2] = a[7]  = [5] + 2
+ *       m[4][3] = a[10] = [5+4] + 1
+ *       m[5][5] = a[14] = [5+4+3+2] + 0
  */
 
 typedef struct LTM
